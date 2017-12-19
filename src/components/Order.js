76690fb2d2fe22518dc8cca51e2e62ee423cf7e4
Murray Williams <murray.williams@ventureweb.com>
2017-12-19 17:33:02 +0200

    implemented state & setup quantity tracking

--- src/components/Order.js
+++ src/components/Order.js
@@ -12,60 +12,95 @@ const coffeeList = {
       "_id": "askjdfhlk",
       "image": espresso,
       "name": "espresso",
-      "price": 22
+      "price": 22,
+      "quantity": 0
     },
     {
       "_id": "werytres",
       "image": americano,
       "name": "americano",
-      "price": 20
+      "price": 20,
+      "quantity": 0
     },
     {
       "_id": "zcxvbbn",
       "image": macciato,
       "name": "macciato",
-      "price": 26
+      "price": 26,
+      "quantity": 0
     },
     {
       "_id": "lkjhgdsre",
       "image": cappuccino,
       "name": "cappuccino",
-      "price": 18
+      "price": 18,
+      "quantity": 0
     },
   ],
-  order: []
 }
 
 class Order extends Component {
 
+  state = {
+    coffees: []
+  }
+
+  componentDidMount() {
+    this.setState ({
+      coffees: coffeeList.coffees.slice()
+    });
+
+  }
+
   handleOrder = (id, action) => {
     const coffeeOrderList = coffeeList.order;
-    const quantity = 0;
+    let currCoffees = this.state.coffees.slice();
+    const findCoffee = currCoffees.findIndex(coffee => coffee._id === id);
+
     if (action === "Inc") {
-      coffeeList.order.push({
-        _id: id,
-        quantity: (!quantity) ? 1 : quantity + 1
-      });
+      currCoffees[findCoffee].quantity ++;
+      console.log((currCoffees[findCoffee].quantity));
     } else {
-      // console.log(coffeeOrderList.indexOf(id));
-      for (let i = 0; i < coffeeOrderList.length; i++) {
-        if (coffeeOrderList[i]._id == id) {
-          coffeeOrderList.splice(i,1);
-        }
+      if (currCoffees[findCoffee].quantity > 0) {
+        currCoffees[findCoffee].quantity --;
+        console.log((currCoffees[findCoffee].quantity));
       }
     }
+    this.setState({
+      coffees: currCoffees
+    });
 
-    console.log(coffeeList.order);
+    // if (action === "Inc") {
+    //   coffeeList.order.push({
+    //     _id: id,
+    //     quantity: (!quantity) ? 1 : quantity + 1
+    //   });
+    // } else {
+    //   // console.log(coffeeOrderList.indexOf(id));
+    //   for (let i = 0; i < coffeeOrderList.length; i++) {
+    //     if (coffeeOrderList[i]._id == id) {
+    //       coffeeOrderList.splice(i,1);
+    //     }
+    //   }
+    // }
+
   }
   renderCoffees = () => {
-    return coffeeList.coffees.map((coffee) => {
+console.log('re-rendering')
+    return this.state.coffees.map((coffee, index) => {
+      let thisQty = 0;
+      console.log(":", this.state.coffees[index].quantity);
       return (
         <div key={coffee._id}>
           <button onClick={() => this.handleOrder(coffee._id, "Dec")}>-</button>
           <button onClick={() => this.handleOrder(coffee._id, "Inc")}>+</button>
           <img src={coffee.image} alt={coffee.name}/>
-          <p>{coffeeList.order.quantity}</p>
           <h4>{coffee.name}</h4>
+          {
+            (this.state.coffees[index].quantity >= 1)
+              ? <p>{this.state.coffees[index].quantity}</p>
+              : ''
+          }
           <p>R {coffee.price}</p>
 
         </div>
